93d6f7eaf94031b7ad6a83b73a1657016d2e295c
SenorBlub <thomasverhappen@Gmail.com>
2024-12-19 15:12:34 +0100

    update rocket config

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,18 @@ mod services;
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![routes::proxy::handle_get, routes::proxy::handle_post, routes::proxy::handle_delete, routes::proxy::handle_put, routes::proxy::authorize_user])
+        .configure(rocket::Config {
+            address: std::net::Ipv4Addr::UNSPECIFIED.into(),
+            port: 8080,
+            ..rocket::Config::default()
+        })
+        .mount("/", 
+            routes![
+                routes::proxy::handle_get, 
+                routes::proxy::handle_post, 
+                routes::proxy::handle_delete, 
+                routes::proxy::handle_put, 
+                routes::proxy::authorize_user
+            ]
+        )
 }
