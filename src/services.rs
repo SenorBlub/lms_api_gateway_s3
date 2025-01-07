64935876ee5cdd5eb9556a35184cbac68211f9ab
SenorBlub <thomasverhappen@Gmail.com>
2025-01-07 10:36:56 +0100

    update services.rs

--- src/services.rs
+++ src/services.rs
@@ -43,38 +43,38 @@ pub fn get_service_config() -> ServiceConfig {
     ServiceConfig {
         auth_ip: get_var_value("AUTH_IP".to_string()),
         auth_port: get_var_value("AUTH_PORT".to_string()),
-        auth: "{auth_ip}:{auth_port}".to_string(), 
+        auth: "{auth_ip}:{auth_port}/auth".to_string(), 
 
         activity_ip: get_var_value("ACTIVITY_IP".to_string()),
         activity_port: get_var_value("ACTIVITY_PORT".to_string()),
-        activity: "{activity_ip}:{activity_port}".to_string(),
+        activity: "{activity_ip}:{activity_port}/activity".to_string(),
 
         ai_ip: get_var_value("AI_IP".to_string()),
         ai_port: get_var_value("AI_PORT".to_string()),
-        ai: "{ai_ip}:{ai_port}".to_string(),
+        ai: "{ai_ip}:{ai_port}/ai".to_string(),
 
         content_ip: get_var_value("CONTENT_IP".to_string()),
         content_port: get_var_value("CONTENT_PORT".to_string()),
-        content: "{content_ip}:{content_port}".to_string(),
+        content: "{content_ip}:{content_port}/content".to_string(),
 
         logging_ip: get_var_value("LOGGING_IP".to_string()),
         logging_port: get_var_value("LOGGING_PORT".to_string()),
-        logging: "{logging_ip}:{logging_port}".to_string(),
+        logging: "{logging_ip}:{logging_port}/logging".to_string(),
 
         live_chat_ip: get_var_value("LIVE_CHAT_IP".to_string()),
         live_chat_port: get_var_value("LIVE_CHAT_PORT".to_string()),
-        live_chat: "{live_chat_ip}:{live_chat_port}".to_string(),
+        live_chat: "{live_chat_ip}:{live_chat_port}/liveChat".to_string(),
 
         notification_ip: get_var_value("NOTIFICATION_IP".to_string()),
         notification_port: get_var_value("NOTIFICATION_PORT".to_string()),
-        notification: "{notification_ip}:{notification_port}".to_string(),
+        notification: "{notification_ip}:{notification_port}/notification".to_string(),
 
         plan_ip: get_var_value("PLAN_IP".to_string()),
         plan_port: get_var_value("PLAN_PORT".to_string()),
-        plan: "{plan_ip}:{plan_port}".to_string(),
+        plan: "{plan_ip}:{plan_port}/plan".to_string(),
 
         user_ip: get_var_value("USER_IP".to_string()),
         user_port: get_var_value("USER_PORT".to_string()),
-        user: "{user_ip}:{user_port}".to_string(),
+        user: "{user_ip}:{user_port}/user".to_string(),
     }
 }
